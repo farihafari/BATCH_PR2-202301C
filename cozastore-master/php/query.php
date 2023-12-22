060548b29c07d9fef4318bb3976e19b9ac638694
farihafari <farihafari594@gmail.com>
2023-12-22 12:53:38 +0500

    add to cart start

--- cozastore-master/php/query.php
+++ cozastore-master/php/query.php
@@ -1,5 +1,18 @@
 <?php
+session_start();
+// session_unset();
 include("php/connection.php");
 $imageCatAdress="../dashmin/img/category/";
-$imageProAdress="../dashmin/img/product/"
+$imageProAdress="../dashmin/img/product/";
+// add to cart;
+if(isset($_POST['addToCart'])){
+    if(isset($_SESSION['cart'])){
+        
+    }else{
+        $_SESSION['cart'][0]=array("pId"=>$_POST['proId'],"pName"=>$_POST['proName'],"pPrice"=>$_POST['proPrice'],"pImage"=>$_POST['proImage'],"pQuantity"=>$_POST['proQuantity']);
+        echo "<script>
+        alert('product add into cart');
+        </script>";
+    }
+}
 ?>
--- cozastore-master/product.php
+++ cozastore-master/product.php
@@ -276,7 +276,8 @@ include("header.php");
 
                     <div class="block2-txt flex-w flex-t p-t-14">
                         <div class="block2-txt-child1 flex-col-l ">
-                            <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                            <a href="product-detail.php?pId=<?php echo $proAll['product_id']?>"
+                                class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
                                 <?php echo $proAll['product_name']?>
                             </a>
 
@@ -298,8 +299,47 @@ include("header.php");
             </div>
             <?php
 	}
-}
-?>
+}else{
+   $query = $pdo ->query("select * from products");
+
+            $row = $query ->fetchAll(PDO::FETCH_ASSOC);
+       foreach($row as $proAll){
+        ?>
+
+            <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+                <!-- Block2 -->
+                <div class="block2">
+                    <div class="block2-pic hov-img0">
+                        <img src="<?php echo $imageProAdress.$proAll['product_image']?>" alt="IMG-PRODUCT">
+
+
+                    </div>
+
+                    <div class="block2-txt flex-w flex-t p-t-14">
+                        <div class="block2-txt-child1 flex-col-l ">
+                            <a href="product-detail.php?pId=<?php echo $proAll['product_id']?>"
+                                class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                <?php echo $proAll['product_name']?>
+                            </a>
+
+                            <span class="stext-105 cl3">
+                                PKR: <?php echo $proAll['product_price']?>
+                            </span>
+                        </div>
+
+                        <div class="block2-txt-child2 flex-r p-t-3">
+                            <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png"
+                                    alt="ICON">
+                                <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png"
+                                    alt="ICON">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php } } ?>
         </div>
 
         <!-- Load more -->
